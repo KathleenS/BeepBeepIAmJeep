d0e421d5dc9e5d4b4758927ad4a67bef5b96547e
Tim Lee <tdlee1230@gmail.com>
2012-11-08 00:23:04 -0800

    Menu and bug fix update
    Contains basic buttons for main menu (without functionality) and fixed a
    bug

--- gen/com/team/beepbeepiamjeep/R.java
+++ gen/com/team/beepbeepiamjeep/R.java
@@ -26,8 +26,11 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int beep_beep_title=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int campaign1=0x7f040002;
+        public static final int campaign2=0x7f040003;
+        public static final int exit_game=0x7f040005;
+        public static final int settings=0x7f040004;
+        public static final int title_activity_main=0x7f040006;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
